480814fced49bd6a8d3a4b6de1dc14b62d031dc7
EiderLabiano <elabianbeg@educacion.navarra.es>
2024-11-15 09:49:45 +0100

    comienzo concesionario

---
+++ src/Empresa.java
@@ -0,0 +1,35 @@
+import javax.swing.text.Keymap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Empresa {
+    private String nombre; // Nombre de la empresa
+    private double facturacionEmpresa; // Dinero vendido
+    private Map<String, Concesionario> grupo; // KeyMap con sede (ciudad) y concesionario
+
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        this.facturacionEmpresa = 0.0;
+        this.grupo = new HashMap<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getFacturacionEmpresa() {
+        return facturacionEmpresa;
+    }
+
+    public void setFacturacionEmpresa(double facturacionEmpresa) {
+        this.facturacionEmpresa = facturacionEmpresa;
+    }
+
+    public Map<String, Concesionario> getGrupo() {
+        return grupo;
+    }
+}
